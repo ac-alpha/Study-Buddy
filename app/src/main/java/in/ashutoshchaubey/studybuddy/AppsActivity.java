f3488d5d36559b2b3da5c3ff511994b58cba6bf2
ac-alpha <ashutoshchaubey2999@gmail.com>
2018-05-31 02:42:10 +0530

    Made basic layout file

---
+++ app/src/main/java/in/ashutoshchaubey/studybuddy/AppsActivity.java
@@ -0,0 +1,13 @@
+package in.ashutoshchaubey.studybuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apps);
+    }
+}
